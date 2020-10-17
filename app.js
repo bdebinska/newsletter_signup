7316f477b0d7c6bd9115677ff86abb9b8752c383
Beata Dębińska <bea.debinska@gmail.com>
2020-10-17 19:00:41 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+  const firstName = req.body.name;
+  const lastName = req.body.surname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us2.api.mailchimp.com/3.0/lists/fe7334d052";
+  const option = {
+    method: "POST",
+    auth: "BEATA:d34d7a444824c555c2300e034d10ea6f"
+  }
+
+  const request = https.request(url, option, function(response){
+
+    if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html")
+    }
+    else {
+        res.sendFile(__dirname + "/failure.html")
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port 3000.");
+});
